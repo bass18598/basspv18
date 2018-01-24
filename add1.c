f7a941d23aebd8b193035baa8a22857c474087fc
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-24 15:45:53 +0530

    add1.c

---
+++ add1.c
@@ -0,0 +1,13 @@
+# #include<stdio.h>
+void main()
+{
+int i,j,k,add;
+printf("Enter the numbers");
+scanf("%d",&j);
+printf("Enter the starting number");
+scanf("%d",&i);
+printf("Enter the difference");
+scanf("%d",&k);
+add=i+j+k;
+printf("%d",add);
+}
