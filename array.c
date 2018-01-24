a875574696e80c0de487cf72690f554164b497a4
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-24 16:54:58 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define max size 100
+int main()
+{
+int arr[max,size];
+int a,n;
+printf("enter array size :");
+scanf("%d",&n);
+printf("enter %d elements :",n);
+for(i=0;i<n;i++)
+printf("%d",arr[a]);
+}
+return 0;
