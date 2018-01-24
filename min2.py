e97eb76777ab2ed898d57dd71ba06b28773802ed
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-24 16:18:05 +0530

    min.py

---
+++ min2.py
@@ -0,0 +1,6 @@
+a = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    a.append(number)
+print("Minimum element in the list is :", min(lst))
