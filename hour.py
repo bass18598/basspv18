b6933230a369e24bceaf7a180007ef9fd46790f6
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-24 17:00:26 +0530

    hour.py

---
+++ hour.py
@@ -0,0 +1,5 @@
+def convert_seconds(seconds):
+    days, seconds = divmod(seconds, 24 * 60 * 60)
+    hours, seconds = divmod(seconds, 60 * 60)
+    minutes, seconds = divmod(seconds, 60)
+    return days, hours, minutes, seconds
