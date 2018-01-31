5486d507decb003dc0ab19777a72704706aecc32
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-31 16:16:17 +0530

    count3.c

---
+++ count3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+ char a[200]={"happy life"};
+ int i,count=0;
+ for(i=0;a[i]!='\0';i++)
+ {
+     if(a[i]==' ')
+        count++;
+ }
+ printf("number of words: %d",count);
+ return 0;
+}
