9eb1578a196848b16dc59178c14d172d609f29f9
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-05 16:03:50 +0530

    print.c

---
+++ print.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char a[30];
+  int i,b;
+  printf("enter the string");
+  scanf("%d,& b");
+  gets(a);
+  for(i=1;i<=3;i++)
+  {
+    puts(a);
+    printf("\n");
+    
+  }
+}
