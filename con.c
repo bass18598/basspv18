44c66703a4b3c6415ef2be5bd79bb94db58a8f87
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-05 16:32:08 +0530

    con.c

---
+++ con.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+	char a[200],b[100];
+	printf("\n enter the 2 strings");
+	scanf("%s %s",&a,&b);
+	printf("\n concadenated string is: %s%s",a,b);
+	return 0;
+  }
