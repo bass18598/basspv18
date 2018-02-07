ed2bc9664db81877891f1834c47ff9bad39c5d92
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-07 16:55:34 +0530

    Create sum1.c

---
+++ sum1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the number a:\n");
+    printf("enter the number b:\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("total is :%d",c);
+}
