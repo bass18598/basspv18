4b01fdaecc0c37b8b030c699aeff6ff997493c3a
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-09 17:00:23 +0530

    Create pro.c

---
+++ pro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the 1st number:");
+	scanf("%d",&n);
+	printf("enter the 2 nd number:");
+	scanf("%d",&i);
+	if((n*i)%2==0)
+	
+	   printf("even number");
+	else
+	   printf("odd number");
+
+  
+}
