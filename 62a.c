2a3e4bf20c893d770c50d87ef7a8644a09dbf1f0
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-19 16:07:44 +0530

    Rename 61a.c to 62a.c

---
+++ 62a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+char a[100],i,flag=1;
+printf("enter the string\n");
+scanf("%s",&a[i]);
+for(i=0;i<a[i];i++)
+{
+if(a[i]=='0'||a[i]=='1')
+  flag=1;
+else
+ flag=0;
+ break;
+}
+{
+  if(a[i]=='0'||a[i]=='1')
+    printf("yes");
+  else
+   printf("no");
+}return 0;
+}
