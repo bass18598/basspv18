adf7da09b97f04051974925a2ff23b61b8ff63f4
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-19 15:39:57 +0530

    Create 61a.c

---
+++ 61a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char s[20];
+int n,i;
+printf("enter the string\n");
+scanf("%s",&s[i]);
+printf("enter the limit:\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+  {
+    printf("%c",s[i]);
+  }
+}
