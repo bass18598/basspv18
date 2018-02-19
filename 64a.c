89bfa67d2c19413ef05c1bb92f41701220eb00f2
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-19 16:40:41 +0530

    Create 64a.c

---
+++ 64a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{int n,m,l;
+printf("enter 1st number:\n");
+scanf("%d",&n);
+printf("enter 2nd number:\n");
+scanf("%d",&m);
+if(((m+n)%2)==0)
+{
+  
+  printf("even number");
+}
+else
+{
+  printf("odd number");
+}
+}
