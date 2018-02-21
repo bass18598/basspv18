d297a16db5df947d53fbe5dc7f2960b83855d757
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-21 16:45:50 +0530

    Rename 67a.c to 68a.c

---
+++ 68a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the numbers:\n");
+scanf("%d",&a);
+if((a%7)==0)
+ printf("%d given number is multiplty of 7",a);
+else
+printf("%d not multiply of 7",a);
+}
