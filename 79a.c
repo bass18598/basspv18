be302ea7e2a9a4fc636cdbe38c4e2602c84e96b3
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-03-02 16:22:26 +0530

    Create 79a.c

---
+++ 79a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the two value\n");
+scanf("%d%d",&a,&b);
+c=a*b;
+printf("%d",c);
+  if((a==b))
+  {
+    printf("\t perfect square");
+  }
+  else
+   printf("\t not perfect square");
+return 0;
+}
