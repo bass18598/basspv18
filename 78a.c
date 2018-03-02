4cec6d73f9b9c72939aeb29fb7d783f66deacd89
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-03-02 16:12:01 +0530

    Create 78a.c

---
+++ 78a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,i;
+printf("enter the value\n");
+scanf("%d",&a);
+  if((a%13)==0)
+  {
+    printf("multiples of 13");
+  }
+  else
+   printf("not multiples of 13");
+return 0;
+}
