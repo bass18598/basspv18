bcc88a27c4173f8aaeac9013b056220e6c9575c6
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-03-14 16:54:00 +0530

    Create 91a.c

---
+++ 91a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,k,s;
+    printf("Enter the length:\n enter the breath:\n enter the height:\n");
+    scanf("%d%d%d",&i,&j,&k);
+    s=i*j*k;
+    printf("surfacr of cuboid is %d:",s);
+}
+  
